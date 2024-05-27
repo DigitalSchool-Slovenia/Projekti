1bc7049b0038465a6c0ccc69817cf62c6beeedd7
Digital School Slovenija <129721339+DigitalSchool-Slovenia@users.noreply.github.com>
2024-05-27 10:13:36 +0200

    Add files via upload

---
+++ zakljucniProjekti2024/HTML/Social_Group/Final_Project.js
@@ -0,0 +1,9 @@
+setTimeout(() => {
+  console.log("amenjekul");
+    const element=document.getElementById('intro');
+    element.id = "animate";
+  }, 2000);
+
+  const slideOut = document.querySelector('div'); 
+
+
